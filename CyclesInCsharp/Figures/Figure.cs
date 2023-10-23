39a696cddaec8933e4289e9d270b87e6367fd408
prizrak107 <prizrak107@DESKTOP-AC7E7UT>
2023-10-23 15:45:25 +0300

    Add project files.

---
+++ CyclesInCsharp/Figures/Figure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyclesInCsharp.Figures
+{
+    internal class Figure: IFigure
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public Figure() { }
+
+        public Figure(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public virtual void SetInputSize() {
+        
+        }
+
+        public virtual void PrintFigure()
+        {
+            Console.WriteLine($"###\nPrint method from base figure class Height={Height} Width={Width}\n###");
+        }
+
+    }
+}
---
+++ CyclesInCsharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using CyclesInCsharp.Figures;
+
+namespace CyclesInCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                int height = 0;
+                int width = 0;
+
+                Console.WriteLine("Write height: ");
+                string heightInput = Console.ReadLine();
+
+                if (int.TryParse(heightInput, out int heightParsed))
+                {
+                    height = heightParsed;
+                }
+                else
+                {
+                    Console.WriteLine($"input width : '{heightInput}' is not correct should be a real number");
+                    continue;
+                }
+
+                Console.WriteLine("Write width: ");
+                string widthInput = Console.ReadLine();
+
+                if (int.TryParse(widthInput, out int widthParsed))
+                {
+                    width = widthParsed;
+                }
+                else
+                {
+                    Console.WriteLine($"input width : '{widthInput}' is not correct should be a real number");
+                    continue;
+                }
+
+                Figure figure = new Figure(height, width);
+
+                figure.PrintFigure();
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
