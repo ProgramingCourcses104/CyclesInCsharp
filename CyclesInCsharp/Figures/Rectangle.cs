efacc9830a6dadfd64883d2b09c91f3d4b884dc6
Valentyn Yarema <valik9988@outlook.com>
2023-10-30 20:31:42 +0200

    add implementation

--- CyclesInCsharp/Figures/Rectangle.cs
+++ CyclesInCsharp/Figures/Rectangle.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace CyclesInCsharp.Figures
 {
-    internal class Rectangle
+    internal class Rectangle : Figure, IFigure
     {
+        public Rectangle() : base() 
+        {
+            Name = "Rectangle";
+
+        }
+
+        public Rectangle (int height, int width) : base (height, width)
+        {
+            Name = "Rectangle";
+        }
+
+
+        public override void PrintFigure()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    int sumIndex = i + j;
+                    if (sumIndex % 2 == 0)
+                    {
+                        ArrayElements[i, j] = "#";
+                    }
+                    else
+                    {
+                        ArrayElements[i, j] = "@";
+
+
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write(ArrayElements[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+
+        public override void PrintFigureArea()
+        {
+            base.PrintFigureArea();
+            Console.WriteLine($"{Name} area={Height*Width}");
+        }
     }
 }
--- CyclesInCsharp/Program.cs
+++ CyclesInCsharp/Program.cs
@@ -6,43 +6,24 @@ namespace CyclesInCsharp
     {
         static void Main(string[] args)
         {
+            List<IFigure> figures = new List<IFigure>() { new Triangle(), new Rectangle(), new Circle() };
+            
 
-            while (true)
+            foreach(var item in figures)
             {
-                int height = 0;
-                int width = 0;
-
-                Console.WriteLine("Write height: ");
-                string heightInput = Console.ReadLine();
-
-                if (int.TryParse(heightInput, out int heightParsed))
-                {
-                    height = heightParsed;
-                }
-                else
-                {
-                    Console.WriteLine($"input width : '{heightInput}' is not correct should be a real number");
-                    continue;
-                }
-
-                Console.WriteLine("Write width: ");
-                string widthInput = Console.ReadLine();
-
-                if (int.TryParse(widthInput, out int widthParsed))
-                {
-                    width = widthParsed;
-                }
-                else
-                {
-                    Console.WriteLine($"input width : '{widthInput}' is not correct should be a real number");
-                    continue;
-                }
-
-                Figure figure = new Figure(height, width);
-
-                figure.PrintFigure();
-
+                item.SetInputSize();
             }
+
+            foreach (var item in figures)
+            {
+                item.PrintFigure();
+            }
+
+            foreach (var item in figures)
+            {
+                item.PrintFigureArea();
+            }
+
 
             Console.ReadKey();
         }
